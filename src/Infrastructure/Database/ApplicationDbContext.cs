c9ba71fa44d6c50b2d740590df66f7b28afc479f
Le Tan Trong <trong.le@enlabsoftware.com>
2022-03-26 01:03:57 +0700

    base database

---
+++ src/Infrastructure/Database/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Database
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            foreach(var entityType in builder.Model.GetEntityTypes())
+            {
+                var tableName = entityType.GetTableName();
+                if (tableName.StartsWith("AspNet"))
+                {
+                    entityType.SetTableName(tableName.Substring(6));
+                }
+            }
+        }
+    }
+}
--- src/WebMVC/Program.cs
+++ src/WebMVC/Program.cs
@@ -1,7 +1,11 @@
+using Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddIdentity<User, IdentityRole>();
 
 var app = builder.Build();
 
